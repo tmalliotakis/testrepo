23d6fdcb3b804e22cdc48bf1b450ad61d4026da2
Thodoris <tmalliotakis@gmail.com>
2017-08-30 14:39:51 +0300

    apples.java added

---
+++ apples.java
@@ -0,0 +1,14 @@
+package testproject;
+
+import java.util.Scanner;
+
+public class apples {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int intinput = input.nextInt();
+		
+		System.out.println("Hello world:"+intinput);
+	}
+
+}
